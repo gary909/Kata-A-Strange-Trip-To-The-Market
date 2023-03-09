3fa79e47abaa63c6594f26f9a0a8ad8af11fc7b1
gary909 <garywhite61@hotmail.com>
2023-03-09 12:42:38 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function isLockNessMonster(s) {
+    //FIND THE LOCH NESS MONSTER. SAVE YOUR TREE FIDDY
+    return s;
+}
+
+console.log(isLockNessMonster("Your girlscout cookies are ready to ship. Your total comes to tree fiddy")); // true
+console.log(isLockNessMonster("Yo, I heard you were on the lookout for Nessie. Let me know if you need assistance.")); // false
